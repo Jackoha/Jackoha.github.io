54f64ddc3872d38f140b7ae92bb5e28ac84eb5ca
Jackoha <jackoha0412@gmail.com>
2026-01-21 20:38:00 +0800

    Site updated: 2026-01-21 20:38:00

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767193383976";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768999074396";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
